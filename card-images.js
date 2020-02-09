3d5734087d7f6eac95b297b321ee77c87b9adeeb
Kewin Dousse <me@protectator.ch>
2020-02-10 00:23:41 +0100

    Scripts to update cards static resources

--- card-images.js
+++ card-images.js
@@ -1,7 +1,9 @@
 import simpleGit from 'simple-git';
 import fs from 'fs';
-import { request } from './request';
-
+import path from 'path';
+// import { request } from './request.js';
+import request from 'request';
+import unzipper from 'unzipper';
 const imagesRepo = simpleGit('./dist');
 
 /*
@@ -55,12 +57,59 @@ const setBundles = [
   'https://dd.b.pvp.net/datadragon-set1-en_us.zip'
 ];
 
-const start = async () => {
-  for (let setUrl in setBundles) {
+(async () => {
+  for (let setUrl of setBundles) {
     const pathName = new URL(setUrl).pathname;
     const fileName = pathName.substring(pathName.lastIndexOf('/') + 1);
-    const data = await request(setUrl);
-    fs.closeSync(fs.openSync(fileName, 'w'));
-    await fs.promises.writeFile(fileName, data);
+    const fileNameWithoutExt = fileName.substring(0, pathName.lastIndexOf('.') - 1);
+
+    const file = fs.createWriteStream(path.join('temp', path.sep, fileName));
+
+    console.log("Downloading...");
+    /* Using Promises so that we can use the ASYNC AWAIT syntax */
+    await new Promise((resolve, reject) => {
+      let stream = request({
+        /* Here you should specify the exact link to the file you are trying to download */
+        uri: setUrl,
+        headers: {
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+          'Accept-Encoding': 'gzip, deflate, br',
+          'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8,ro;q=0.7,ru;q=0.6,la;q=0.5,pt;q=0.4,de;q=0.3',
+          'Cache-Control': 'max-age=0',
+          'Connection': 'keep-alive',
+          'Upgrade-Insecure-Requests': '1',
+          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+        },
+        /* GZIP true for most of the websites now, disable it if you don't need it */
+        gzip: true
+      })
+        .pipe(file)
+        .on('finish', () => {
+          console.log(`The file is finished downloading.`);
+          resolve();
+        })
+        .on('error', (error) => {
+          reject(error);
+        })
+    })
+      .catch(error => {
+        console.log(`Something happened: ${error}`);
+      });
+    console.log("Finished downloading with success. Unzipping...");
+
+    await new Promise((resolve, reject) => {
+      fs.createReadStream(path.join('temp', path.sep, fileName))
+        .pipe(unzipper.Extract({ path: path.join('dist', path.sep, fileNameWithoutExt) })
+          .on('close', () => {
+            console.log('Done !');
+            resolve();
+          })
+          .on('error', (e) => {
+            console.log('Sad : ', e);
+            reject();
+          })
+        );
+    });
   }
-};
+})();
+
