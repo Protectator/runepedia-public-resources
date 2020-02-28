19a486c0ae0c64818c4523f3e7106723f16f9403
Kewin Dousse <me@protectator.ch>
2020-02-28 19:10:31 +0100

    WIP

---
+++ scripts/clone.sh
@@ -0,0 +1,5 @@
+mkdir dist
+cd dist
+git clone https://github.com/Protectator/runepedia-public-resources-static.git .
+git config --global user.email "action@github.com"
+git config --global user.name "Github Action"
